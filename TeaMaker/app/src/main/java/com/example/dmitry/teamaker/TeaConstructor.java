9bfc5ba54cedc194fd4428cb862262e30d53a3da
Dmitry <antrdima@gmail.com>
2019-01-02 18:20:09 +0500

    Init TeaMaker

---
+++ TeaMaker/app/src/main/java/com/example/dmitry/teamaker/TeaConstructor.java
@@ -0,0 +1,13 @@
+package com.example.dmitry.teamaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeaConstructor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tea_constructor);
+    }
+}
