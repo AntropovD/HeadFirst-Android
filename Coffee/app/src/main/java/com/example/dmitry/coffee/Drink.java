f4c813c56d74c3a26b66569f8cd1302bf7ad6cf4
Dmitry <antrdima@gmail.com>
2019-01-02 21:43:55 +0500

    Basic coffee app

---
+++ Coffee/app/src/main/java/com/example/dmitry/coffee/Drink.java
@@ -0,0 +1,22 @@
+package com.example.dmitry.coffee;
+
+public class Drink {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    public static final Drink[] drinks = {
+            new Drink("Latte", "A couple of espresso shots with steamed milk",
+                    R.drawable.latte),
+            new Drink("Cappuccino", "Espresso, hot milk, and a steamed milk foam",
+                    R.drawable.cappuccino),
+            new Drink("Filter", "Highest quality beans roasted and brewed fresh",
+                    R.drawable.filter)
+    };
+
+    public Drink(String name, String description, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+}
