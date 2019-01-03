3fc3276b3215414ffb8dc802c779f95239caec53
Dmitry <antrdima@gmail.com>
2019-01-03 21:25:21 +0500

    MainActivity different on phone and tablet

--- Workout/app/src/main/java/com/example/dmitry/workout/MainActivity.java
+++ Workout/app/src/main/java/com/example/dmitry/workout/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.dmitry.workout;
 
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
     implements WorkoutListFragment.WorkoutListListener {
@@ -15,12 +17,19 @@ public class MainActivity extends AppCompatActivity
 
   @Override
   public void itemClicked(long id) {
-    WorkoutDetailFragment fragment = new WorkoutDetailFragment();
-    FragmentTransaction transaction = getFragmentManager().beginTransaction();
-    fragment.setWorkout(id);
-    transaction.replace(R.id.fragment_container, fragment);
-    transaction.addToBackStack(null);
-    transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-    transaction.commit();
+    View fragmentContainer = findViewById(R.id.fragment_container);
+    if (fragmentContainer != null) {
+      WorkoutDetailFragment fragment = new WorkoutDetailFragment();
+      FragmentTransaction transaction = getFragmentManager().beginTransaction();
+      fragment.setWorkout(id);
+      transaction.replace(R.id.fragment_container, fragment);
+      transaction.addToBackStack(null);
+      transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+      transaction.commit();
+    } else {
+      Intent intent = new Intent(this, DetailActivity.class);
+      intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
+      startActivity(intent);
+    }
   }
 }
