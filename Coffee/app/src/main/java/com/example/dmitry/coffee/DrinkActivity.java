2afa5e770f4e642b7cea72a51e07f043c6b24fbe
Dmitry <antrdima@gmail.com>
2019-01-10 16:26:52 +0500

    Chapter 12 use SimpleCursorAdapter to get data from db

--- Coffee/app/src/main/java/com/example/dmitry/coffee/DrinkActivity.java
+++ Coffee/app/src/main/java/com/example/dmitry/coffee/DrinkActivity.java
@@ -1,30 +1,53 @@
 package com.example.dmitry.coffee;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DrinkActivity extends AppCompatActivity {
 
-    public static final String EXTRA_DRINKNO = "drinkNo";
+  public static final String EXTRA_DRINKNO = "drinkNo";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drink);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_drink);
 
-        int drinkNo = (Integer) getIntent().getExtras().get(EXTRA_DRINKNO);
-        Drink drink = Drink.drinks[drinkNo];
+    int drinkNo = (Integer) getIntent().getExtras().get(EXTRA_DRINKNO);
+
+    try {
+      SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+      SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
+      Cursor cursor = db.query("DRINK",
+          new String[]{"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"}, "_id = ?",
+          new String[]{Integer.toString(drinkNo)}, null, null, null
+      );
+      if (cursor.moveToFirst()) {
+        String nameText = cursor.getString(0);
+        String descriptionText = cursor.getString(1);
+        int resourceId = cursor.getInt(2);
+
 
         ImageView photo = findViewById(R.id.photo);
-        photo.setImageResource(drink.getImageResourceId());
-        photo.setContentDescription(drink.getName());
+        photo.setImageResource(resourceId);
+        photo.setContentDescription(nameText);
 
         TextView name = findViewById(R.id.name);
-        name.setText(drink.getName());
+        name.setText(nameText);
 
         TextView description = findViewById(R.id.description);
-        description.setText(drink.getDescription());
+        description.setText(descriptionText);
+      }
+      cursor.close();
+      db.close();
+    } catch (Exception e) {
+      Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+      toast.show();
     }
+  }
 }
--- Coffee/app/src/main/java/com/example/dmitry/coffee/DrinkCategoryActivity.java
+++ Coffee/app/src/main/java/com/example/dmitry/coffee/DrinkCategoryActivity.java
@@ -2,31 +2,61 @@ package com.example.dmitry.coffee;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class DrinkCategoryActivity extends ListActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  private SQLiteDatabase db;
+  private Cursor cursor;
 
-        ListView listDrinks = getListView();
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                Drink.drinks);
-        listDrinks.setAdapter(listAdapter);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-    public void onListItemClick(ListView listView,
-                                View itemView,
-                                int position,
-                                long id) {
-        Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
-        intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
-        startActivity(intent);
+    ListView listDrinks = getListView();
+
+    try {
+      SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+      db = starbuzzDatabaseHelper.getReadableDatabase();
+
+      cursor = db.query("DRINK", new String[]{"id", "NAME"}, null, null, null, null, null);
+
+      CursorAdapter listAdapter = new SimpleCursorAdapter(this,
+          android.R.layout.simple_list_item_2,
+          cursor,
+          new String[]{"Name"},
+          new int[]{android.R.id.text1}, 0);
+
+      listDrinks.setAdapter(listAdapter);
+
+    } catch (SQLiteException e) {
+      Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+      toast.show();
     }
+  }
+
+  public void onListItemClick(ListView listView,
+      View itemView,
+      int position,
+      long id) {
+    Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+    intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+    startActivity(intent);
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    cursor.close();
+    db.close();
+  }
 }
