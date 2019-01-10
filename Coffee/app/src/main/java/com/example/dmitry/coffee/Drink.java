b7a9efc958382985a361505da25569e23d952065
Dmitry <antrdima@gmail.com>
2019-01-10 14:21:06 +0500

    warning fixes

--- Coffee/app/src/main/java/com/example/dmitry/coffee/Drink.java
+++ Coffee/app/src/main/java/com/example/dmitry/coffee/Drink.java
@@ -2,39 +2,40 @@ package com.example.dmitry.coffee;
 
 import android.support.annotation.NonNull;
 
-public class Drink {
-    static final Drink[] drinks = {
-            new Drink("Latte", "A couple of espresso shots with steamed milk",
-                    R.drawable.latte),
-            new Drink("Cappuccino", "Espresso, hot milk, and a steamed milk foam",
-                    R.drawable.cappuccino),
-            new Drink("Filter", "Highest quality beans roasted and brewed fresh",
-                    R.drawable.filter)
-    };
-    private String name;
-    private String description;
-    private int imageResourceId;
+class Drink {
 
-    private Drink(String name, String description, int imageResourceId) {
-        this.name = name;
-        this.description = description;
-        this.imageResourceId = imageResourceId;
-    }
+  static final Drink[] drinks = {
+      new Drink("Latte", "A couple of espresso shots with steamed milk",
+          R.drawable.latte),
+      new Drink("Cappuccino", "Espresso, hot milk, and a steamed milk foam",
+          R.drawable.cappuccino),
+      new Drink("Filter", "Highest quality beans roasted and brewed fresh",
+          R.drawable.filter)
+  };
+  private final String name;
+  private final String description;
+  private final int imageResourceId;
 
-    public String getDescription() {
-        return description;
-    }
+  private Drink(String name, String description, int imageResourceId) {
+    this.name = name;
+    this.description = description;
+    this.imageResourceId = imageResourceId;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    int getImageResourceId() {
-        return imageResourceId;
-    }
+  public String getName() {
+    return name;
+  }
 
-    @NonNull
-    public String toString() {
-        return this.name;
-    }
+  int getImageResourceId() {
+    return imageResourceId;
+  }
+
+  @NonNull
+  public String toString() {
+    return this.name;
+  }
 }
